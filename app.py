3c0bd2246dbf75a4742e50c2946aca0ddced7071
ChocomintX <401162659@qq.com>
2021-03-22 20:15:22 +0800

    创建了新项目 用于图书馆相关功能

---
+++ app.py
@@ -0,0 +1,56 @@
+import json
+
+from flask import Flask
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/autolibrary/api/searchSeat')
+def searchSeat():
+    headers = {
+        'Host': 'xzxt.hhtc.edu.cn',
+        'Connection': 'keep-alive',
+        'Content-Length': '64',
+        'Accept': 'application/json',
+        'Origin': 'http://xzxt.hhtc.edu.cn',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1316.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Referer': 'http://xzxt.hhtc.edu.cn/mobile/html/seat/seatdate.html?v=20191117&seataddress=DZXS306&seatdate=2021-03-22&mapid=27&isloadstatus=0',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.5;q=0.4',
+        'Cookie': 'ASP.NET_SessionId=oypn1lynbdgrfqetmn4am2ka; txw_cookie_txw_unit_Id=968131EA0968E222; dt_cookie_user_name_remember=1B4CDA49ECF4477CF326B0E02FF26E86'
+    }
+
+    data = {
+        'data_type': 'getSeatDate',
+        'seatno': 'HHXYTSG20620099',
+        'seatdate': '2021-03-22'
+    }
+
+    r = requests.post('http://xzxt.hhtc.edu.cn/mobile/ajax/seat/SeatDateHandler.ashx', headers=headers, data=data)
+    str = json.loads(r.text)['data']
+    data = []
+    if len(str) != 0:
+        data = json.loads(str)
+
+    result = {}
+    for i in data:
+        startTime = datetime.strptime(i['StartTime'], '%Y/%m/%d %H:%M:%S')
+        endTime = datetime.strptime(i['EndTime'], '%Y/%m/%d %H:%M:%S')
+
+        print(startTime)
+        print(endTime)
+        if startTime < datetime.now() < endTime:
+            result['startTime'] = i['StartTime']
+            result['endTime'] = i['EndTime']
+            result['id'] = i['reader_no']
+            result['department'] = i['department_name']
+            result['name'] = i['real_name']
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    app.run()
